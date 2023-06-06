22105a54b5fdaa48da37e40948a007cc43198c36
robinGiri <girirobin2018@gmail.com>
2023-06-06 15:42:17 +0545

    chore: database connected

---
+++ src/database/DbConnection.java
@@ -0,0 +1,117 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package database;
+
+
+
+import javax.swing.*;
+
+import java.sql.*;
+
+
+
+public class DbConnection {
+
+    public Connection connection;
+
+    Statement statement;
+
+    ResultSet resultSet;
+
+    int value;
+
+
+
+    public DbConnection(){
+
+        try {
+
+            String username = "root";
+
+            String password = "Kavi123%4";
+
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            connection = DriverManager.getConnection(
+
+                    "jdbc:mysql://localhost:3306/LED",username,password);
+
+
+
+                    if(connection!=null){
+
+                        System.out.println("Connected to database");
+
+                    }else{
+
+                        System.out.println("Error connecting to database");
+
+                    }
+
+            statement = connection.createStatement();
+
+        }catch (Exception e){
+
+            e.printStackTrace();
+
+        }
+
+    }
+
+
+
+    // Via the use of sql query
+
+    // insert, update and delete
+
+    public int manipulate(String query){
+
+        try {
+
+            value = statement.executeUpdate(query);
+
+            connection.close();
+
+        }catch (SQLIntegrityConstraintViolationException ex){
+
+            JOptionPane.showMessageDialog(null, "These details already exist!");
+
+        }catch (SQLException e){
+
+            e.printStackTrace();
+
+        }
+
+        return value;
+
+    }
+
+
+
+    public ResultSet retrieve(String query){
+
+        try {
+
+            resultSet = statement.executeQuery(query);
+
+        }catch (SQLException e){
+
+            e.printStackTrace();
+
+        }
+
+        return resultSet;
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        DbConnection db = new DbConnection();
+
+    }
+
+}
