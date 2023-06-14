d79711356acf309bf60f0bc51890afce64ae09f4
Robin Giri <45058288+robinGiri@users.noreply.github.com>
2023-06-14 13:47:42 +0545

    chore: admin service added (#6)

--- src/controller/RegisterController.java
+++ src/controller/RegisterController.java
@@ -1,36 +1,14 @@
 package controller;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import database.DbConnection;
+import model.Admin;
+import service.AdminService;
 
 public class RegisterController {
-    private DbConnection dbConnection = new DbConnection();
-    private Connection connection;
-    private PreparedStatement preparedStatement;
+    AdminService AdminService = new AdminService();
 
     public Boolean register(String username, String password, String email) {
-
-        connection = dbConnection.connection;
-        String query = "insert into users (username, pass, email) values (?, ?, ?)";
-        try {
-            preparedStatement = connection.prepareStatement(query);
-            preparedStatement.setString(1, username);
-            preparedStatement.setString(2, password);
-            preparedStatement.setString(3, email);
-            int status = preparedStatement.executeUpdate();
-
-            if(status > 0){
-                return(true);
-            }
-
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return(false);
-
+        Admin admin = new Admin(username, password, email);
+        return AdminService.register(admin);
     }
-    
 
 }
