d8b396fc30ac123a5cc60923382af8a236a9176a
Sandesh Nilas <sandeshnk019382@nec.edu.np>
2023-06-15 22:28:55 +0545

    feat: subscription service and controller added

---
+++ src/controller/SubscriptionController.java
@@ -0,0 +1,31 @@
+package controller;
+
+import model.Subscription;
+import service.SubscriptionServices;
+
+
+public class SubscriptionController {
+    SubscriptionServices subscriptionservices = new SubscriptionServices();
+    
+    //Insert Subscription by user;
+    public boolean createSubscription(int subscriptionId, String type, double discountRate){
+        Subscription subscription = new Subscription(subscriptionId, type, discountRate);
+        return subscriptionservices.createSubscription(subscription);
+        
+    }
+    //Update Subscription by user;
+    public boolean updateSubscription(int subscriptionId, String type, double discountRate){
+        Subscription subscription = new Subscription(subscriptionId, type, discountRate);
+        return subscriptionservices.updateSubscription(subscription);
+    }
+    
+    //Show list of the subscription type;
+    public void showSubscriptionList(){
+        for (Subscription sublist :  subscriptionservices.listSubscription()){
+            System.out.println(sublist.getType()+ "\t");
+            System.out.println(sublist.getDiscountRate() + "\t");
+            System.out.println(sublist.getSubscriptionId()+"\n");
+            
+        }
+    }
+}
---
+++ src/service/SubscriptionServices.java
@@ -0,0 +1,103 @@
+package service;
+
+import java.sql.*;
+import database.DbConnection;
+import model.Subscription;
+import java.util.*;
+
+public class SubscriptionServices {
+    private PreparedStatement preparedStatement;
+    private Connection connection;
+    private ResultSet resultSet;
+    private DbConnection dbConnection = new DbConnection();
+
+    // It checks the table and create new tables if not exists
+    public boolean CheckTableSubscription() {
+        connection = dbConnection.connection;
+        String query = "CREATE TABLE IF NOT EXISTS subscription (" +
+                "subscriptionId INT PRIMARY KEY, " +
+                "type VARCHAR(25), " +
+                "discountRate double)";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.execute();
+            return (true);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return (false);
+
+    }
+
+    // Create subscription
+    public boolean createSubscription(Subscription subscription) {
+        CheckTableSubscription();
+        connection = dbConnection.connection;
+        String query = "INSERT INTO subscription(subscriptionId,type,discountRate) values (?,?,?)";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, Integer.toString(subscription.getSubscriptionId()));
+            preparedStatement.setString(2, subscription.getType());
+            preparedStatement.setString(3, Double.toString(subscription.getDiscountRate()));
+            int status = preparedStatement.executeUpdate();
+            System.out.println(status);
+            return (true);
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+
+        }
+        return (false);
+
+    }
+
+    public boolean updateSubscription(Subscription subscription) {
+        connection = dbConnection.connection;
+        String query = "UPDATE subscription SET (subscriptionId = ?,type = ?,discountRate = ?)";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, Integer.toString(subscription.getSubscriptionId()));
+            preparedStatement.setString(2, query);
+            preparedStatement.setString(3, Double.toString(subscription.getDiscountRate()));
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public boolean deleteSubscription(Subscription subscription) {
+        connection = dbConnection.connection;
+        String query = "DELETE FROM subscription where subscriptionId=?;";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, Integer.toString(subscription.getSubscriptionId()));
+            return true;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return false;
+    }
+
+    public ArrayList<Subscription> listSubscription() {
+        ArrayList<Subscription> subscriptionslist = new ArrayList<Subscription>();
+        connection = dbConnection.connection;
+        String query = "SELECT * FROM subscription";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+            while (resultSet.next()) {
+                int SubId = Integer.parseInt(resultSet.getString("subscriptionId"));
+                String type = resultSet.getString("type");
+                double discount = Double.parseDouble(resultSet.getString("discountRate"));
+                Subscription subscription = new Subscription(SubId, type, discount);
+                subscriptionslist.add(subscription);
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return subscriptionslist;
+    }
+}
