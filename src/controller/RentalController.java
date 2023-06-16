80d0c5ec041becf0240b1aacc3c10eee08c20849
Sandesh Nilas <sandeshnk019382@nec.edu.np>
2023-06-16 23:32:32 +0545

    feat: added payment services and controller

--- src/controller/RentalController.java
+++ src/controller/RentalController.java
@@ -13,8 +13,8 @@ public class RentalController {
     }
 
     public boolean createRental(int rentalId, java.util.Date rentalDateTime, java.util.Date returnDateTime,
-                                int totalPrice, int ownerId, int carId, int categoryId, int customerId,
-                                String rentalStatus, String pickup, String dropoff, int driverId, int paymentId) {
+            int totalPrice, int ownerId, int carId, int categoryId, int customerId,
+            String rentalStatus, String pickup, String dropoff, int driverId, int paymentId) {
         Rental rental = new Rental(rentalId, new java.sql.Date(rentalDateTime.getTime()),
                 new java.sql.Date(returnDateTime.getTime()), totalPrice, ownerId, carId, categoryId, customerId,
                 rentalStatus, pickup, dropoff, driverId, paymentId);
@@ -22,8 +22,8 @@ public class RentalController {
     }
 
     public boolean updateRental(int rentalId, java.util.Date rentalDateTime, java.util.Date returnDateTime,
-                                int totalPrice, int ownerId, int carId, int categoryId, int customerId,
-                                String rentalStatus, String pickup, String dropoff, int driverId, int paymentId) {
+            int totalPrice, int ownerId, int carId, int categoryId, int customerId,
+            String rentalStatus, String pickup, String dropoff, int driverId, int paymentId) {
         Rental rental = new Rental(rentalId, new java.sql.Date(rentalDateTime.getTime()),
                 new java.sql.Date(returnDateTime.getTime()), totalPrice, ownerId, carId, categoryId, customerId,
                 rentalStatus, pickup, dropoff, driverId, paymentId);
@@ -37,10 +37,6 @@ public class RentalController {
     public void showRentalList() {
         ArrayList<Rental> rentalsList = rentalService.listRentals();
         for (Rental rental : rentalsList) {
-            System.out.println(rental.getRental_id() + " | " + rental.getRental_datetime() + " | "
-                    + rental.getReturn_datetime() + " | " + rental.getTotal_price() + " | " + rental.getOwner_id()
-                    + " | " + rental.getCar_id() + " | " + rental.getCategory_id() + " | " + rental.getCustomer_id() + " | " + rental.getRental_status() + " | " + rental.getPickup() + " | " + rental.getDropoff()
-                    + " | " + rental.getDriver_id() + " | " + rental.getPayment_id());
         }
     }
 }
