80ab928809eb5ac78494181855887e298fda178d
Ukran004 <110590995+Ukran004@users.noreply.github.com>
2023-06-22 00:25:04 +0545

    feat:count service added (#25)

---
+++ src/service/CarCountService.java
@@ -0,0 +1,61 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import database.DbConnection;
+
+public class CarCountService {
+    private PreparedStatement preparedStatement;
+    private Connection connection;
+    private ResultSet resultSet;
+    private DbConnection dbConnection = new DbConnection();
+
+    public int countAvailableCars() {
+        connection = dbConnection.connection;
+        int count = 0;
+
+        String query = "SELECT COUNT(carId) AS NumberOfCar FROM car WHERE carStatus = 'Available'";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                count = resultSet.getInt("NumberOfCar");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            closeResources();
+        }
+
+        return count;
+    }
+
+    private void closeResources() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error while closing resources: " + e.getMessage());
+        }
+    }
+    public static void main(String[] args) {
+    CarCountService carCountService = new CarCountService();
+    int numberOfAvailableCars = carCountService.countAvailableCars();
+    System.out.println("Number of available cars: " + numberOfAvailableCars);
+
+ 
+}
+
+}
+
---
+++ src/service/PaymentCountService.java
@@ -0,0 +1,60 @@
+package service;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import database.DbConnection;
+
+public class PaymentCountService {
+    private PreparedStatement preparedStatement;
+    private Connection connection;
+    private ResultSet resultSet;
+    private DbConnection dbConnection = new DbConnection();
+
+    public double countTotalPaymentAmount() {
+        connection = dbConnection.connection;
+        double totalAmount = 0.0;
+
+        String query = "SELECT SUM(paymentAmount) AS TotalPaymentAmount FROM payment";
+        try {
+            preparedStatement = connection.prepareStatement(query);
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                totalAmount = resultSet.getDouble("TotalPaymentAmount");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            closeResources();
+        }
+
+        return totalAmount;
+    }
+
+    private void closeResources() {
+        try {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error while closing resources: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        PaymentCountService paymentCountService = new PaymentCountService();
+        double totalPaymentAmount = paymentCountService.countTotalPaymentAmount();
+        System.out.println("Total payment amount: " + totalPaymentAmount);
+
+    }
+    
+}
