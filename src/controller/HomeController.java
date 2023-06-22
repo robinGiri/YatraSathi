dbf8954c8e10e41858507c02c96fc9d0afe1b8ca
ksandresh1 <51807706+ksandresh1@users.noreply.github.com>
2023-06-22 07:59:11 +0545

    Feat: Added the bardiagram in the homepage of the project (#26)
    Co-authored-by: Sandesh Nilas <sandeshnk019382@nec.edu.np>

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -1,5 +1,16 @@
 package controller;
 
+import service.HomeService;
+import model.Income;
+import java.util.ArrayList;
+
 public class HomeController {
-    
+    HomeService homeService;
+    public HomeController(){
+        homeService = new HomeService();
+    }
+    public ArrayList<Income> showIncomeData(){
+        ArrayList<Income> incomeList = homeService.calculateTotalIncome();
+        return incomeList;      
+    }
 }
---
+++ src/model/Income.java
@@ -0,0 +1,39 @@
+package model;
+public class Income {
+
+    public Income(int rental_year, int rental_month, double total_income) {
+        this.rental_year = rental_year;
+        this.rental_month = rental_month;
+        this.total_income = total_income;
+    }
+      public Income() {
+    }
+
+    public int getRental_year() {
+        return rental_year;
+    }
+
+    public void setRental_year(int rental_year) {
+        this.rental_year = rental_year;
+    }
+
+    public int getRental_month() {
+        return rental_month;
+    }
+
+    public void setRental_month(int rental_month) {
+        this.rental_month = rental_month;
+    }
+
+    public double getTotal_income() {
+        return total_income;
+    }
+
+    public void setTotal_income(double total_income) {
+        this.total_income = total_income;
+    }
+    int rental_year;
+    int rental_month;
+    double total_income;
+    
+}
